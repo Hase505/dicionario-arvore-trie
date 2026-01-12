938291b5e6186c40a3defe679afaaa2ecbf9e6de
Erick Winck <erickwinck5@gmail.com>
2026-01-12 01:13:38 +0000

    feat: adicionar funções auxiliares para string (#2)
    Adiciona as funções:
    - palavra_valida: verifica se contém apenas letras.
    - trim: remove espaços em branco no início e final da string.
    - string_para_minusculo: converte todos os caracteres para minúsculo.

---
+++ include/util.h
@@ -0,0 +1,34 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+/**
+ * @file util.h
+ * @brief Definição das funções auxiliares.
+ */
+
+/**
+ * @brief Verifica se a palavra informada é válida.
+ *
+ * Verifica se todos os caracteres são letras (maiúsculas e minúsculas).
+ *
+ * @param str String a ser validada.
+ *
+ * @return 1 se a palavra for válida ou 0 se for inválida.
+ */
+int palavra_valida(const char* str);
+
+/**
+ * @brief Remove espaços à esquerda e à direita da string.
+ *
+ * @param str String a ser alterada.
+ */
+void trim(char* str);
+
+/*
+ * @brief Converte a string para minúsculo.
+ *
+ * @param str String a ser convertida.
+ */
+void string_para_minusculo(char* str);
+
+#endif
