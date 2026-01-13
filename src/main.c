c73bd9245db200c5cd87f210c6ff5ba6a2ff0e4a
Erick Winck <erickwinck5@gmail.com>
2026-01-13 19:18:52 -0300

    feat: adicionar testes básicos na main

--- src/main.c
+++ src/main.c
@@ -1,6 +1,26 @@
+#include <stddef.h>
 #include <stdio.h>
+
+#include "trie.h"
 
 int main() {
     printf("Hello World\n");
+
+    no_trie* raiz = trie_criar();
+    trie_inserir(raiz, "abacaxi");
+    trie_inserir(raiz, "antes");
+    trie_inserir(raiz, "romulo");
+    trie_inserir(raiz, "arte");
+
+    size_t quantidade = 0;
+    char** palavras = trie_listar_palavras(raiz, &quantidade);
+
+    for (size_t i = 0; i < quantidade; i++) {
+        printf("%s\n", palavras[i]);
+    }
+
+    trie_liberar_lista(palavras, quantidade);
+    trie_destruir(raiz);
+
     return 0;
 }
