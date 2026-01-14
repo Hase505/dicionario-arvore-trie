1373c1d08f36f995d491a6b1c3572d21b70d3751
Erick Winck <erickwinck5@gmail.com>
2026-01-14 19:17:53 -0300

    feat: adicionar testes na main

--- src/main.c
+++ src/main.c
@@ -1,12 +1,50 @@
-
-#include "trie.h"
 #include <stddef.h>
 #include <stdio.h>
 
 #include "trie.h"
 
 int main() {
-    printf("Hello World\n");
+    no_trie* raiz = trie_criar();
+
+    trie_inserir(raiz, "abacaxi");
+    trie_inserir(raiz, "abertura");
+    trie_inserir(raiz, "romulo");
+    trie_inserir(raiz, "antedeguemon");
+
+    size_t quantidade = 0;
+    char** palavras = trie_listar_palavras(raiz, &quantidade);
+
+    printf("Todas as palavras:\n");
+    for (size_t i = 0; i < quantidade; i++) {
+        printf("%s\n", palavras[i]);
+    }
+    printf("\n");
+
+    trie_liberar_lista(palavras, quantidade);
+    quantidade = 0;
+
+    trie_remover(raiz, "abertura");
+    palavras = trie_listar_palavras(raiz, &quantidade);
+
+    printf("Todas as palavras após remover 'abertura':\n");
+    for (size_t i = 0; i < quantidade; i++) {
+        printf("%s\n", palavras[i]);
+    }
+    printf("\n");
+
+    trie_liberar_lista(palavras, quantidade);
+    quantidade = 0;
+
+    palavras = trie_buscar_por_prefixo(raiz, "a", &quantidade);
+
+    printf("Todas as palavras após buscar por prefixo 'a':\n");
+    for (size_t i = 0; i < quantidade; i++) {
+        printf("%s\n", palavras[i]);
+    }
+    printf("\n");
+
+    trie_liberar_lista(palavras, quantidade);
+    trie_destruir(raiz);
 
     return 0;
 }
