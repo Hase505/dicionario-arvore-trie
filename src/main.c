7526289b24c034b24c049490f46cd4cab479e21e
lrivas01 <lucasrn015@gmail.com>
2026-01-14 08:57:54 -0300

    feat: adicionar testes na main

--- src/main.c
+++ src/main.c
@@ -1,3 +1,5 @@
+
+#include "trie.h"
 #include <stddef.h>
 #include <stdio.h>
 
@@ -7,23 +9,60 @@ int main() {
     printf("Hello World\n");
 
     no_trie* raiz = trie_criar();
+
     trie_inserir(raiz, "abacaxi");
     trie_inserir(raiz, "antes");
     trie_inserir(raiz, "romulo");
     trie_inserir(raiz, "arte");
 
     size_t quantidade = 0;
-    char** palavras = trie_listar_palavras(raiz, &quantidade);
 
+    char** palavras = trie_listar_palavras(raiz, &quantidade);
+    printf("--- Antes da remocao ---\n");
     for (size_t i = 0; i < quantidade; i++) {
         printf("%s\n", palavras[i]);
     }
 
     trie_liberar_lista(palavras, quantidade);
+    printf("\nRemovendo 'romulo'...\n");
+    trie_remover(raiz, "romulo");
+    palavras = trie_listar_palavras(raiz, &quantidade);
+    printf("--- Depois da remocao ---\n");
+    for (size_t i = 0; i < quantidade; i++) {
+        printf("%s\n", palavras[i]);
+    }
 
-    printf("Busca pelo prefixo 'a':\n");
-    palavras = trie_buscar_por_prefixo(raiz, "a", &quantidade);
+    trie_liberar_lista(palavras, quantidade);
+    printf("\nRemovendo 'antes'...\n");
+    trie_remover(raiz, "antes");
+    palavras = trie_listar_palavras(raiz, &quantidade);
+    printf("--- Depois da remocao ---\n");
+    for (size_t i = 0; i < quantidade; i++) {
+        printf("%s\n", palavras[i]);
+    }
 
+    trie_liberar_lista(palavras, quantidade);
+    printf("\nRemovendo 'abacaxi'...\n");
+    trie_remover(raiz, "abacaxi");
+    palavras = trie_listar_palavras(raiz, &quantidade);
+    printf("--- Depois da remocao ---\n");
+    for (size_t i = 0; i < quantidade; i++) {
+        printf("%s\n", palavras[i]);
+    }
+
+    trie_liberar_lista(palavras, quantidade);
+    printf("\nRemovendo 'arte'...\n");
+    trie_remover(raiz, "arte");
+    palavras = trie_listar_palavras(raiz, &quantidade);
+    printf("--- Depois da remocao ---\n");
+    for (size_t i = 0; i < quantidade; i++) {
+        printf("%s\n", palavras[i]);
+    }
+    trie_liberar_lista(palavras, quantidade);
+    printf("\nRemovendo 'abacaxi'...\n");
+    trie_remover(raiz, "abacaxi");
+    palavras = trie_listar_palavras(raiz, &quantidade);
+    printf("--- Depois da remocao ---\n");
     for (size_t i = 0; i < quantidade; i++) {
         printf("%s\n", palavras[i]);
     }
